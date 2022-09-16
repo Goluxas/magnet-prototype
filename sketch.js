03924be9f1c29c4b905fc4807b0ae37687529007
Douglas Storz <douglasstorz@gmail.com>
2022-09-16 13:33:00 -0400

    Propulsion is now analog

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function setup() {
     x: canvasWidth / 2,
     y: canvasHeight / 2,
     size: 20,
-    speed: 5,
+    speed: 5.0,
   }
 }
 
@@ -32,22 +32,23 @@ function propel() {
     return;
   }
 
-  // basic movment with no normalization
-  // ie. will move faster diagonally than in cardinals
-  x_speed = 0;
-  y_speed = 0;
-  if (mouseX > player.x) {
-    x_speed = -player.speed;
-  } else if (mouseX < player.x) {
-    x_speed = player.speed;
-  }
+  // vector movement time!
+  player_vector = createVector(player.x, player.y);
+  mouse_vector = createVector(mouseX, mouseY);
 
-  if (mouseY > player.y) {
-    y_speed = -player.speed;
-  } else if (mouseY < player.y) {
-    y_speed = player.speed;
-  }
+  move_vector = p5.Vector.sub(player_vector, mouse_vector);
 
-  player.x += x_speed;
-  player.y += y_speed;
+  // using p5's angleBetween
+  //angle = move_vector.angleBetween(createVector(1,0));
+  // NOTE: this made the player move away on Y-axis and toward on X-axis
+  // not sure why atan2 works but this doesn't
+
+  // using atan2
+  angle = Math.atan2(move_vector.y, move_vector.x);
+
+  dx = player.speed * Math.cos(angle);
+  dy = player.speed * Math.sin(angle);
+
+  player.x += dx;
+  player.y += dy;
 }
